2bb0e1fa77f0472efa7378c332a9d77da146bcb8
tecnologia-ibtech <31856009+tecnologia-ibtech@users.noreply.github.com>
2020-03-23 09:25:33 -0300

    Alteração Entrypoint.sh XDEBUG

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -70,6 +70,21 @@ EOF
 
 create_user_from_directory_owner "/var/www/html"
 
+set -e
+echo "[ ****************** ] Starting Endpoint of Application"
+
+echo "[ ****************** ] Downloading composer "
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+echo "[ ****************** ] Installing composer "
+php composer-setup.php
+
+echo "[ ****************** ] Unlinking and moving composer to '/usr/local/bin/' directory"
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
 # X-Debug
 
 if ! [ -v $XDEBUG_REMOTE_ENABLE ] ; then
@@ -104,4 +119,5 @@ if ! [ -v $XDEBUG_REMOTE_ENABLE ] ; then
 	)
 fi
 
+echo "[ ****************** ] Ending Endpoint of Application"
 exec "$@"
